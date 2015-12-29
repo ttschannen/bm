36c74dd773c87e20d1b067f5b48f32937a76b609
deploy.sh <t@ivi.admin.ch>
2015-12-29 19:18:22 +0100

    added bin/mkbmidx.pl
    The new script generates a asciidoc file with an index of the bookmark directory.
    This index will be found at http://ttschannen.github.io/bm/bmi

---
+++ bin/mkbmidx.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+#
+#
+#
+my $DEBUG=0;
+my $bdir="content";
+
+open(BMFILES,"ls -1 $bdir/bm_*.xml|");
+
+open(I,">$bdir/bmi.asciidoc")||die "cannot open $bdir/index.asiidoc for writing";
+
+
+    print I <<"EndOfHeader";
++++
+date = "2015-12-29T08:36:54-07:00"
+draft = false
+title = "Index"
+
++++
+= Index
+
+We have these bookmarkfiles:
+
+EndOfHeader
+
+
+while(<BMFILES>){
+	chop;
+	$_=~/bm_(\w+)\.xml$/;
+	$fn=$1;
+	printf (I "* http://ttschannen.github.io/bm/bm_%s.xml[%s]\n",$fn,$fn);
+}
+    print I <<'EndOfTrailer';
+
+Enjoy!
+EndOfTrailer
